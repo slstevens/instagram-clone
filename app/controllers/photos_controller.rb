ead0e6d4f34a4bf784c938d33182dd58e74c9f76
slstevens <sean.stevens.123@gmail.com>
2014-11-08 13:14:23 +0000

    first commit

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,30 @@
+class PhotosController < ApplicationController
+
+	def index
+		@photos = Photo.all
+  end
+
+  def new
+  	@photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.create(photo_params)
+    redirect_to '/photos'
+  end
+
+  def photo_params
+    params.require(:photo).permit(:title)
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+    @photo.update(photo_params)
+    redirect_to '/photos'
+  end
+
+end
---
+++ spec/features/photos_feature_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+describe 'photos' do
+	context 'no photos have been added' do
+		it 'should display a prompt to add a photo' do
+			visit '/photos'
+			expect(page).to have_content 'No photos'
+			expect(page).to have_link 'Add a photo'
+		end
+	end
+
+	context 'photos have been added' do
+		before do
+			Photo.create(title: 'Hello World')
+		end
+
+		it 'should display photos' do
+			visit '/photos'
+			expect(page).to have_content('Hello World')
+			expect(page).not_to have_content('No photos yet')
+		end
+	end
+end
+
+describe 'creating photos' do
+	it 'prompts user to fill out a form, then displays the new photo' do
+		visit '/photos'
+		click_link 'Add a photo'
+		fill_in 'Title', with: 'Hello World'
+		click_button 'Create Photo'
+		expect(page).to have_content 'Hello World'
+		expect(current_path).to eq '/photos'
+	end
+end
+
+describe 'editing photos' do
+
+	before do
+		Photo.create(title:'Hello World Before Edit')
+	end
+
+	it 'lets a user edit a Photo' do
+		visit '/photos'
+		click_link 'Edit'
+		fill_in 'Title', with: 'Hello World'
+		click_button 'Update Photo'
+		expect(page).to have_content 'Hello World'
+		expect(current_path).to eq '/photos'
+	end
+
+end
