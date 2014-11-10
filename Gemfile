a5c54dc6886f70dd8845f65ac6702267f222aae0
slstevens <sean.stevens.123@gmail.com>
2014-11-10 10:47:40 +0000

    Returned CSS to normal

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise', git: 'https://github.com/plataformatec/devise.git', :branch => 'lm-rails-4-2'
 gem 'omniauth-facebook'
 gem 'paperclip'
+gem 'cloudinary'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -8,6 +8,6 @@ class Photo < ActiveRecord::Base
 
 	validates :title, length: {minimum: 3}
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :image, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
