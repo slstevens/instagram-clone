496e5c04c301f8f3c9414b3907d309e4ee0eb8bf
slstevens <sean.stevens.123@gmail.com>
2014-11-10 15:50:00 +0000

    Added face recognition and glasses adding

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,17 +4,18 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   include CarrierWave::RMagick
+  include Cloudinary::CarrierWave
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -25,16 +26,47 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :scale => [200, 300]
+  process :scale => [400]
+  # process :convert => 'png'
   
   def scale(width, height)
     # do something
   end
 
+  def public_id
+   Cloudinary::PreloadedFile.split_format(original_filename).first + "_" + Cloudinary::Utils.random_public_id[0,6]
+  end
+  #Later I guess you can change this to the title of the photo
+
   # Create different versions of your uploaded files:
   version :thumb do
     # process :resize_to_fit => [50, 50]
     process :resize_to_limit => [100, 100]
+  end
+
+  version :bright_face do
+    cloudinary_transformation :effect => "brightness:30", :radius => 20,
+      :width => 100, :height => 150, :crop => :thumb, :gravity => :face
+  end
+
+  # version :moustache_face do
+  #   cloudinary_transformation :sign_url => true, :overlay => "FunnyGlasses_xljond", 
+  #   :width => 1.7, :flags => "region_relative", :gravity => "rek_eyes", :y => 20
+  # end
+
+  version :moustache_face do
+    cloudinary_transformation :sign_url => true, :overlay => "moustacheBrown_nv8jae", 
+    :width => 0.7, :flags => "region_relative", :gravity => "face", :y => 50
+  end
+
+  version :glasses_face do
+    cloudinary_transformation :sign_url => true, :overlay => "FunnyGlasses_xljond", 
+    :width => 0.9, :flags => "region_relative", :gravity => "face"
+  end
+
+  version :glasses_facev2 do
+    cloudinary_transformation :sign_url => true, :overlay => "FunnyGlasses_xljond", 
+    :width => 1.7, :flags => "region_relative", :gravity => "rek_eyes", :y => 10
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
